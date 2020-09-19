511bde52fbe3f321d6c9a911ad0ab8326679edfc
Rifki Amdan <rifki.amdan@gmail.com>
2020-09-19 23:26:16 +0700

    commit kontak

--- app/src/main/java/com/example/sidomulyomelayani/MainActivity.java
+++ app/src/main/java/com/example/sidomulyomelayani/MainActivity.java
@@ -2,32 +2,136 @@ package com.example.sidomulyomelayani;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
+    private TextView tv_web_title;
     public String url="https://www.sidomulyoaman.com";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        webView = (WebView) findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-//        webView.loadUrl("https://www.sidomulyoaman.com");
+        tv_web_title = findViewById(R.id.tv_web_title);
+        webView = (WebView) findViewById(R.id.webView);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+////        setContentView(webView);
 //        webView.setWebViewClient(new WebViewClient());
+//        webView.loadUrl("https://www.sidomulyoaman.com");
 
 
-        webView= findViewById(R.id.webView);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setSupportMultipleWindows(false);
-        webView.setWebViewClient(new myWebClient());
-        webView.loadUrl(url);
+        webView.getSettings().setJavaScriptEnabled(true); // enable javascript
 
+//        final Activity activity = this;
+//
+//        webView.setWebViewClient(new WebViewClient() {
+//            @SuppressWarnings("deprecation")
+//            @Override
+//            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+//                Toast.makeText(MainActivity.this, description, Toast.LENGTH_SHORT).show();
+//            }
+//            @TargetApi(android.os.Build.VERSION_CODES.M)
+//            @Override
+//            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+//                // Redirect to deprecated method, so you can use it in all SDK versions
+//                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+//            }
+//        });
+//
+//        webView.loadUrl("https://www.sidomulyoaman.com");
+//        setContentView(webView);
+
+//        webView= findViewById(R.id.webView);
+//        WebSettings webSettings = webView.getSettings();
+//        webSettings.setSupportMultipleWindows(false);
+//        webView.setWebViewClient(new myWebClient());
+//        webView.loadUrl(url);
+
+        webView.setWebChromeClient( new MyWebChromeClient());
+        webView.setWebViewClient( new webClient());
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.sidomulyoaman.com");
+
+        tv_web_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone ="6282336365060";
+                boolean isWhatsappInstalled = whatsappInstalledOrNot("com.whatsapp");
+                if (isWhatsappInstalled) {
+                    Intent sendIntent = new Intent("android.intent.action.MAIN");
+                    sendIntent.setComponent(new ComponentName("com.whatsapp", "com.whatsapp.Conversation"));
+                    sendIntent.putExtra("jid", PhoneNumberUtils.stripSeparators(phone) + "@s.whatsapp.net");
+                    startActivity(sendIntent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Whatsapp tidak terinstall", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    private boolean whatsappInstalledOrNot(String uri) {
+        PackageManager pm = getPackageManager();
+        boolean app_installed = false;
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            app_installed = false;
+        }
+        return app_installed;
+    }
+
+
+    public class MyWebChromeClient extends WebChromeClient {
+        public void onProgressChanged(WebView view, int newProgress) {
+//            progressBar.setVisibility(View.VISIBLE);
+//            progressBar.setProgress(newProgress);
+        }
+    }
+
+    public class webClient extends WebViewClient {
+        public boolean  shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+//            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+//            progressBar.setVisibility(View.GONE);
+        } else  {
+            super.onBackPressed();
+        }
     }
 
     public class myWebClient extends WebViewClient
@@ -48,12 +152,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
-    }
 }
